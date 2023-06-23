72bb5a405e7eb9cf8a6f5eebbd984ce4c1e4d5cc
Tamil <3149159425@qq.com>
2023-06-24 00:21:54 +0800

    first version for Server Form

---
+++ behavior_pack/ServerForm/modConfig.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+class SystemName:
+    engine = "Engine"
+    server = "ServerForm"
+    client = "ServerForm"
+
+
+class Namespace:
+    engine = "Minecraft"
+    server = "WindX"
+    client = "WindX"
+
+
+class ClassPath:
+    server = "ServerForm.server.main.Main"
+    client = "ServerForm.client.main.Main"
+
+
+class UI:
+    namespace = "ServerForm"
+    key = "Form"
+    classPath = "ServerForm.client.uiScript.main.Main"
+    jsonPath = "ServerForm.Form"
+    createParams = {"isHud": 1}
